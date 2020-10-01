b88e0e701d8919a31740fb5141cd2a37e6ad8313
revelation <nusktecsoft@gmail.com>
2020-10-01 17:19:02 +0100

    Update Corrected 78 - Role Models Managed (Q&A Solved).

--- routes/qanswer.js
+++ routes/qanswer.js
@@ -8,6 +8,7 @@ let util = require('../utils/utils');
 //models
 let manswer = require('./../models/manswer');
 let mpoll = require('./../models/mpolls');
+let dbconn = require('./../models/mconnects');
 
 /* create user. */
 router.all('/create', function (req, res, next) {
@@ -50,6 +51,23 @@ router.all('/list', function (req, res, next) {
             }).catch(err => {
             util.Jwr(res, false, [], "Error listening responses");
         })
+    }, true)
+});
+
+/* List all answers from users */
+router.all('/list-title', function (req, res, next) {
+    util.JSONChecker(res, req.body, async (data) => {
+        if(data.qtitle && data.qtitle!==''){
+            //read mysql raw
+            const resp = await dbconn.query("select * from `rs_manswers` ans RIGHT JOIN `rs_mpolls` pol ON pol.qid=ans.aqid where pol.qtitle='"+data.qtitle+"'");
+            if(resp && resp[0].length>0){
+                util.Jwr(res, true, resp[0], "Polling listed as required !");
+            }else{
+                util.Jwr(res, false, [], "No poll / survey were answered");
+            }
+            return;
+        }
+        util.Jwr(res, false, [], "Unable to list responses !");
     }, true)
 });
 
